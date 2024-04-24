a7717e30642d1e40cf9a147816b6f0964061b24c
Prem0561 <premkumar82713@gmail.com>
2024-04-24 20:09:56 +0530

    first file

---
+++ float.py
@@ -0,0 +1,13 @@
+
+def addition(num1, num2):
+    add = (num1 + num2)
+    return(add)
+
+def mul(num1, num2):
+    m = (num1 * num2)
+    return(m)
+
+x = mul (1.5, 2.4)
+y = addition(2, 2.2)
+print("Value of x is:",x, "Value of y is", y)
+
